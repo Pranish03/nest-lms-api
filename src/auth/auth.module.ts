13ecdbac6df037df6071efb20b9f933ee223d6b7
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-09-02 16:14:13 +0545

    Refactor code structure for improved readability and maintainability

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
+import { UserModule } from '../user/user.module.js';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  imports: [UserModule],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service.js';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
   registerUser() {
+    this.userService.createUser();
+
     return { message: 'User registered successfully' };
   }
 }
