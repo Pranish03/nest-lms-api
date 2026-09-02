08a6d0b74cba7ed8031a3438f388184a6883976f
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-09-02 18:02:34 +0545

    feat: create user schema and use it inside createUser service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,19 @@
+import { InjectModel } from '@nestjs/mongoose';
 import { RegisterDto } from './../auth/dto/register.dto.js';
 import { Injectable } from '@nestjs/common';
+import { User } from './schemas/user.schema.js';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
-  createUser(registerDto: RegisterDto) {
-    return { message: 'User created!' };
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async createUser(registerDto: RegisterDto) {
+    return await this.userModel.create({
+      fname: registerDto.fname,
+      lname: registerDto.lname,
+      email: registerDto.email,
+      password: registerDto.password,
+    });
   }
 }
