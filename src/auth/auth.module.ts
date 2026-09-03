3285cbe827bcba1135dcfe151803f9a04827fa18
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-09-03 14:11:41 +0545

    feat: generate jwt token using @nestjs/jwt library

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
 import { UserModule } from '../user/user.module.js';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [UserModule],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { RegisterDto } from './dto/register.dto.js';
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service.js';
 import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async registerUser(registerDto: RegisterDto) {
     const hash = await bcrypt.hash(registerDto.password, 10);
@@ -15,6 +19,11 @@ export class AuthService {
       password: hash,
     });
 
-    return user;
+    const payload = { sub: user._id };
+    const token = await this.jwtService.signAsync(payload);
+
+    console.log(token);
+
+    return token;
   }
 }
